f9eda3f75b5a5dba301326521b7b736ceba05c4f
Josh Wilsdon <jwilsdon@joyent.com>
2015-11-18 11:05:04 -0800

    DOCKER-620 /lib/sdc/docker/logger should be a mount instead of a copy

--- smf/dockerlogger-setup.sh
+++ smf/dockerlogger-setup.sh
@@ -12,13 +12,25 @@
 set -o errexit
 set -o xtrace
 
+# Ensure the directory exists.
 mkdir -p /lib/sdc/docker
-if [[ ! -f /lib/sdc/docker/logger ]] \
-    || ! cmp /opt/smartdc/docker/bin/dockerlogger /lib/sdc/docker/logger; then
 
-    cp /opt/smartdc/docker/bin/dockerlogger /lib/sdc/docker/logger.new \
-        && mv /lib/sdc/docker/logger.new /lib/sdc/docker/logger
+if ! mount | grep "^/lib/sdc/docker/logger"; then
+    # Existing /lib/sdc/docker/logger would not be a mount but a file. This
+    # might be left behind from a previous setup where we copied the file
+    # instead of mounting. In any case we'll remove it.
+    rm -f /lib/sdc/docker/logger
 fi
-chmod 0755 /lib/sdc/docker/logger
+
+# We need this to exist so we can mount over it
+if [[ ! -e /lib/sdc/docker/logger ]]; then
+    # create a file so we can mount over it
+    touch /lib/sdc/docker/logger
+fi
+
+# Now we know that we have /lib/sdc/docker/logger and it's either an empty file
+# we just created, or it's a previous mount. In either case, we'll just mount
+# over it now.
+mount -O -r -F lofs /opt/smartdc/docker/bin/dockerlogger /lib/sdc/docker/logger
 
 exit 0
